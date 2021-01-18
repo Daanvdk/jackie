34b48eb7bfa50ddcaba105f3858922be4ba9e002
Daan van der Kallen <mail@daanvdk.com>
2021-01-18 18:50:55 +0100

    Add stream

---
+++ jack/http/stream.py
@@ -0,0 +1,46 @@
+import asyncio
+import json
+
+
+async def iterable_to_async_iterable(iterable):
+    for value in iterable:
+        yield value
+
+
+class Stream:
+
+    def __init__(self, chunks=b''):
+        if isinstance(chunks, str):
+            chunks = chunks.encode()
+        if isinstance(chunks, bytes):
+            chunks = [chunks]
+        if not hasattr(chunks, '__aiter__'):
+            chunks = iterable_to_async_iterable(chunks)
+        self._cache = []
+        self._chunks = chunks.__aiter__()
+
+    async def chunks(self):
+        index = 0
+        while True:
+            if index < len(self._cache):
+                task = self._cache[index]
+            else:
+                task = asyncio.create_task(self._chunks.__anext__())
+                self._cache.append(task)
+            try:
+                yield await task
+            except StopAsyncIteration:
+                break
+            index += 1
+
+    async def content(self):
+        chunks = []
+        async for chunk in self.chunks():
+            chunks.append(chunk)
+        return b''.join(chunks)
+
+    async def text(self):
+        return (await self.content()).decode()
+
+    async def json(self):
+        return json.loads(await self.content())
---
+++ tests/http/test_stream.py
@@ -0,0 +1,185 @@
+import asyncio
+import pytest
+
+from jack.http.stream import Stream
+
+
+@pytest.mark.asyncio
+async def test_async_chunks_to_chunks():
+    async def async_chunks():
+        yield b'foo'
+        yield b'bar'
+    stream = Stream(async_chunks())
+    chunks = []
+    async for chunk in stream.chunks():
+        chunks.append(chunk)
+    assert chunks == [b'foo', b'bar']
+
+
+@pytest.mark.asyncio
+async def test_chunks_to_chunks():
+    def chunks():
+        yield b'foo'
+        yield b'bar'
+    stream = Stream(chunks())
+    chunks = []
+    async for chunk in stream.chunks():
+        chunks.append(chunk)
+    assert chunks == [b'foo', b'bar']
+
+
+@pytest.mark.asyncio
+async def test_bytes_to_chunks():
+    stream = Stream(b'foobar')
+    chunks = []
+    async for chunk in stream.chunks():
+        chunks.append(chunk)
+    assert chunks == [b'foobar']
+
+
+@pytest.mark.asyncio
+async def test_str_to_chunks():
+    stream = Stream('foobar')
+    chunks = []
+    async for chunk in stream.chunks():
+        chunks.append(chunk)
+    assert chunks == [b'foobar']
+
+
+@pytest.mark.asyncio
+async def test_async_chunks_to_content():
+    async def async_chunks():
+        yield b'foo'
+        yield b'bar'
+    stream = Stream(async_chunks())
+    content = await stream.content()
+    assert content == b'foobar'
+
+
+@pytest.mark.asyncio
+async def test_chunks_to_content():
+    def chunks():
+        yield b'foo'
+        yield b'bar'
+    stream = Stream(chunks())
+    content = await stream.content()
+    assert content == b'foobar'
+
+
+@pytest.mark.asyncio
+async def test_bytes_to_content():
+    stream = Stream(b'foobar')
+    content = await stream.content()
+    assert content == b'foobar'
+
+
+@pytest.mark.asyncio
+async def test_str_to_content():
+    stream = Stream('foobar')
+    content = await stream.content()
+    assert content == b'foobar'
+
+
+@pytest.mark.asyncio
+async def test_async_chunks_to_text():
+    async def async_chunks():
+        yield b'foo'
+        yield b'bar'
+    stream = Stream(async_chunks())
+    text = await stream.text()
+    assert text == 'foobar'
+
+
+@pytest.mark.asyncio
+async def test_chunks_to_text():
+    def chunks():
+        yield b'foo'
+        yield b'bar'
+    stream = Stream(chunks())
+    text = await stream.text()
+    assert text == 'foobar'
+
+
+@pytest.mark.asyncio
+async def test_bytes_to_text():
+    stream = Stream(b'foobar')
+    text = await stream.text()
+    assert text == 'foobar'
+
+
+@pytest.mark.asyncio
+async def test_str_to_text():
+    stream = Stream('foobar')
+    text = await stream.text()
+    assert text == 'foobar'
+
+
+@pytest.mark.asyncio
+async def test_async_chunks_to_json():
+    async def async_chunks():
+        yield b'{'
+        yield b'"foo"'
+        yield b': '
+        yield b'"bar"'
+        yield b'}'
+    stream = Stream(async_chunks())
+    data = await stream.json()
+    assert data == {'foo': 'bar'}
+
+
+@pytest.mark.asyncio
+async def test_chunks_to_json():
+    def chunks():
+        yield b'{'
+        yield b'"foo"'
+        yield b': '
+        yield b'"bar"'
+        yield b'}'
+    stream = Stream(chunks())
+    data = await stream.json()
+    assert data == {'foo': 'bar'}
+
+
+@pytest.mark.asyncio
+async def test_bytes_to_json():
+    stream = Stream(b'{"foo": "bar"}')
+    data = await stream.json()
+    assert data == {'foo': 'bar'}
+
+
+@pytest.mark.asyncio
+async def test_str_to_json():
+    stream = Stream('{"foo": "bar"}')
+    data = await stream.json()
+    assert data == {'foo': 'bar'}
+
+
+@pytest.mark.asyncio
+async def test_stream_multiple_chunks():
+    stream = Stream([b'foo', b'bar', b'baz'])
+    chunks1 = stream.chunks()
+    chunks2 = stream.chunks()
+
+    chunk1 = await chunks1.__anext__()
+    chunk2 = await chunks2.__anext__()
+
+    assert chunk1 == b'foo'
+    assert chunk2 == b'foo'
+
+    chunk1 = asyncio.create_task(chunks1.__anext__())
+    chunk2 = await chunks2.__anext__()
+    chunk1 = await chunk1
+
+    assert chunk1 == b'bar'
+    assert chunk2 == b'bar'
+
+    chunk1 = await chunks1.__anext__()
+    chunk2 = await chunks2.__anext__()
+
+    assert chunk1 == b'baz'
+    assert chunk2 == b'baz'
+
+    with pytest.raises(StopAsyncIteration):
+        await chunks1.__anext__()
+    with pytest.raises(StopAsyncIteration):
+        await chunks2.__anext__()
