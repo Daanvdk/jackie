4d5064e551284d1d5252b71b1088b8c04dee4c1c
Daan van der Kallen <mail@daanvdk.com>
2021-01-21 10:40:11 +0100

    Refactor matcher to use full regex

--- jackie/router/matcher.py
+++ jackie/router/matcher.py
@@ -7,92 +7,89 @@ PARAM_RE = re.compile(PARAM_PATTERN)
 
 
 # mapping of name -> 3 tuple consisting of:
-#   - a regex to match the param
+#   - a pattern to match the param
 #   - a parser to parse the param to a value
 #   - a serializer to serialize a value back to a param
 PARAM_TYPES = {
-    'str': (re.compile(r'[^//]+'), str, str),
-    'int': (re.compile(r'\d+'), int, str),
-    'path': (re.compile(r'.+'), str, str),
+    'str': (r'[^//]+', str, str),
+    'int': (r'\d+', int, str),
+    'path': (r'.+', str, str),
 }
 
 
 class Matcher:
 
     def __init__(self, pattern=''):
-        if isinstance(pattern, str):
-            parts = []
-            index = 0
-            for match in PARAM_RE.finditer(pattern):
-                if index != match.start():
-                    parts.append(('str', pattern[index:match.start()]))
-                name, param_type = match.groups()
-                if param_type is None:
-                    param_type = 'str'
-                if param_type not in PARAM_TYPES:
-                    raise ValueError(f'invalid param type: {param_type}')
-                parts.append(('param', name, param_type))
-                index = match.end()
-            if index != len(pattern):
-                parts.append(('str', pattern[index:]))
-        else:
-            parts = pattern
-        self.parts = parts
+        parts = []
+        params = []
+
+        index = 0
+
+        for match in PARAM_RE.finditer(pattern):
+            if index != match.start():
+                parts.append(re.escape(pattern[index:match.start()]))
+
+            name, param_type = match.groups()
+            if param_type is None:
+                param_type = 'str'
+            try:
+                param_pattern, _, _ = PARAM_TYPES[param_type]
+            except KeyError:
+                raise ValueError(f'invalid param type: {param_type}') from None
+            parts.append(f'({param_pattern})')
+            params.append((name, param_type))
+
+            index = match.end()
+
+        if index != len(pattern):
+            parts.append(re.escape(pattern[index:]))
+
+        print('PATTERN', repr(pattern), repr(r''.join(parts)))
+
+        self.regex = re.compile(r''.join(parts))
+        self.params = params
 
     def __add__(self, other):
         if not isinstance(other, Matcher):
             raise TypeError
-        left = list(self.parts)
-        right = other.parts
-        if left and left[-1][0] == 'str' and right and right[0][0] == 'str':
-            right = iter(right)
-            left[-1] = ('str', left[-1][1] + next(right)[1])
-        left.extend(right)
-        return Matcher(left)
+        merged = Matcher()
+        merged.regex = re.compile(self.regex.pattern + other.regex.pattern)
+        merged.params = [*self.params, *other.params]
+        return merged
 
     def match(self, path):
+        match = self.regex.fullmatch(path)
+        if match is None:
+            raise self.Error
         params = {}
-        index = 0
-
-        for part in self.parts:
-            if part[0] == 'str':
-                _, content = part
-                if not path.startswith(content, index):
-                    raise self.Error(f'{index}: expected {content!r}')
-                index += len(content)
-
-            elif part[0] == 'param':
-                _, name, param_type = part
-                param_re, parse, serialize = PARAM_TYPES[param_type]
-                match = param_re.match(path, index)
-                if not match:
-                    raise self.Error(f'{index}: expected {param_type}')
-                params[name] = parse(match.group())
-                index = match.end()
-
-            else:  # pragma: no cover
-                raise ValueError(f'invalid part: {part[0]}')
-
-        if index != len(path):
-            raise self.Error(f'{index}: expected end of path')
-
+        for (name, param_type), value in zip(self.params, match.groups()):
+            _, parse, _ = PARAM_TYPES[param_type]
+            params[name] = parse(value)
         return params
 
     def reverse(self, **params):
         parts = []
 
-        for part in self.parts:
-            if part[0] == 'str':
-                _, content = part
-                parts.append(content)
+        chars = iter(self.regex.pattern)
+        param_index = 0
+        depth = 0
 
-            elif part[0] == 'param':
-                _, name, param_type = part
-                param_re, parse, serialize = PARAM_TYPES[param_type]
-                parts.append(serialize(params[name]))
-
-            else:  # pragma: no cover
-                raise ValueError(f'invalid part: {part[0]}')
+        for char in chars:
+            if char == '(':
+                depth += 1
+            elif char == ')':
+                assert depth > 0
+                depth -= 1
+                if depth == 0:
+                    name, param_type = self.params[param_index]
+                    _, _, serialize = PARAM_TYPES[param_type]
+                    parts.append(serialize(params[name]))
+                    param_index += 1
+                    continue
+            elif char == '\\':
+                char = next(chars)
+            if depth == 0:
+                parts.append(char)
 
         return ''.join(parts)
 
