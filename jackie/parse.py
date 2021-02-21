3f5a2a14c7d3d5f6275e70f94648ed2f3f1f37f4
Daan van der Kallen <mail@daanvdk.com>
2021-02-21 22:53:16 +0100

    Add support for parsing and serializing cookies

--- jackie/parse.py
+++ jackie/parse.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 import functools
 
 
@@ -55,7 +56,7 @@ def parser(parse):
     @functools.wraps(parse)
     def wrapped_parse(content, *args, **kwargs):
         if isinstance(content, (str, bytes)):
-            content = list(lex(content)), 0
+            content = list(lex(content)), 0, set()
             content, result = parse(content, *args, **kwargs)
             parse_token(content, 'end')
             return result
@@ -66,18 +67,113 @@ def parser(parse):
 
 @parser
 def parse_token(content, *keys, optional=False):
-    tokens, index = content
+    tokens, index, checked = content
     key, i, _ = tokens[index]
     if key in keys:
-        return (tokens, index + 1), tokens[index]
+        return (tokens, index + 1, set()), tokens[index]
+    checked = checked | set(keys)
     if optional:
-        return content, None
-    keys = list(map(repr, sorted(keys)))
+        return (tokens, index, checked), None
+    keys = list(map(repr, sorted(checked)))
     if len(keys) == 1:
         keys = keys[0]
     else:
         keys = ', '.join(keys[:-1]) + ' or ' + keys[-1]
     raise ValueError(f'{i}: got {key!r}, expected {keys}')
+
+
+@parser
+def parse_int(content):
+    index = content[1]
+    content, name = parse_name(content)
+    try:
+        return content, int(name)
+    except ValueError:
+        raise ValueError(f'{index}: expected an int') from None
+
+
+def normalize_enum(value):
+    return value.lower().replace('-', '').replace('_', '')
+
+
+@parser
+def parse_enum(content, *values):
+    index = content[1]
+    content, name = parse_name(content)
+    name = normalize_enum(name)
+    values = {normalize_enum(value): value for value in values}
+    try:
+        return content, values[name]
+    except KeyError:
+        raise ValueError(f'{index}: unexpected value: {name!r}') from None
+
+
+WEEKDAYS = {
+    'mon': 0,
+    'tue': 1,
+    'wed': 2,
+    'thu': 3,
+    'fri': 4,
+    'sat': 5,
+    'sun': 6,
+}
+MONTHS = {
+    'jan': 1,
+    'feb': 2,
+    'mar': 3,
+    'apr': 4,
+    'may': 5,
+    'jun': 6,
+    'jul': 7,
+    'aug': 8,
+    'sep': 9,
+    'oct': 10,
+    'nov': 11,
+    'dec': 12,
+}
+
+
+@parser
+def parse_date(content):
+    index = content[1]
+
+    content, weekday = parse_enum(content, *WEEKDAYS)
+    weekday = WEEKDAYS[weekday]
+    content, _ = parse_token(content, ',')
+    content, day = parse_int(content)
+    content, month = parse_enum(content, *MONTHS)
+    month = MONTHS[month]
+    content, year = parse_int(content)
+    content, hour = parse_int(content)
+    content, _ = parse_token(content, ':')
+    content, minute = parse_int(content)
+    content, _ = parse_token(content, ':')
+    content, second = parse_int(content)
+    content, _ = parse_enum(content, 'GMT', 'UTC')
+
+    try:
+        date = datetime(
+            year, month, day, hour, minute, second, tzinfo=timezone.utc,
+        )
+        assert date.weekday() == weekday
+    except (ValueError, AssertionError):
+        raise ValueError(f'{index}: invalid date') from None
+
+    return content, date
+
+
+@parser
+def parse_path(content):
+    _, token = parse_token(content, 'string', optional=True)
+    if token:
+        return parse_value(content)
+    parts = []
+    while True:
+        content, token = parse_token(content, 'name', '/', optional=True)
+        if token is None:
+            break
+        parts.append(token[2])
+    return content, ''.join(parts)
 
 
 @parser
@@ -135,3 +231,58 @@ def parse_content_type(content):
     content, media_type = parse_media_type(content)
     content, params = parse_params(content)
     return content, (media_type, params)
+
+
+@parser
+def parse_cookies(content):
+    cookies = {}
+    while True:
+        _, token = parse_token(content, 'end', optional=True)
+        if token is not None:
+            break
+        content, name = parse_name(content)
+        content, _ = parse_token(content, '=')
+        content, value = parse_value(content)
+        cookies[name] = value
+        content, token = parse_token(content, ';', optional=True)
+        if token is None:
+            break
+    return content, cookies
+
+
+@parser
+def parse_set_cookie(content):
+    content, cookie_name = parse_name(content)
+    content, _ = parse_token(content, '=')
+    content, cookie_value = parse_value(content)
+
+    params = {'secure': False, 'http_only': False}
+    content, more_params = parse_token(content, ';', optional=True)
+    while more_params:
+        content, param_name = parse_enum(
+            content, 'expires', 'max_age', 'domain', 'path', 'secure',
+            'http_only', 'same_site'
+        )
+        if param_name == 'expires':
+            content, _ = parse_token(content, '=')
+            content, param_value = parse_date(content)
+        elif param_name == 'max_age':
+            content, _ = parse_token(content, '=')
+            content, param_value = parse_int(content)
+        elif param_name == 'domain':
+            content, _ = parse_token(content, '=')
+            content, param_value = parse_value(content)
+        elif param_name == 'path':
+            content, _ = parse_token(content, '=')
+            content, param_value = parse_path(content)
+        elif param_name == 'secure':
+            param_value = True
+        elif param_name == 'http_only':
+            param_value = True
+        elif param_name == 'same_site':
+            content, _ = parse_token(content, '=')
+            content, param_value = parse_enum(content, 'lax', 'strict', 'none')
+        params[param_name] = param_value
+        content, more_params = parse_token(content, ';', optional=True)
+
+    return content, (cookie_name, cookie_value, params)
