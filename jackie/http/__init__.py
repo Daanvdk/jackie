e67db3a82120e49d2d3a0447ab28e86cf1a9fcfd
Daan van der Kallen <mail@daanvdk.com>
2021-02-27 11:17:48 +0100

    Export Disconnect from jackie.http

--- jackie/http/__init__.py
+++ jackie/http/__init__.py
@@ -1,4 +1,5 @@
 from .cookie import Cookie
+from .exceptions import Disconnect
 from .request import Request
 from .response import Response
 from .socket import Socket
@@ -8,6 +9,7 @@ from .wrappers import asgi_to_jackie, jackie_to_asgi
 __all__ = [
     'asgi_to_jackie',
     'Cookie',
+    'Disconnect',
     'jackie_to_asgi',
     'Request',
     'Response',
